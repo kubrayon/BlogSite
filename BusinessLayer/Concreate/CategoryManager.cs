4dca844cf38a635385a23f73bcd2347e3f0c6e26
Kubra Yon <kubrayon98@gmail.com>
2021-10-31 17:10:49 +0300

    added

---
+++ BusinessLayer/Concreate/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concreate.Repositories;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concreate
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName== "" || p.CategoryName.Length <=2 ||  p.CategoryDescription=="" || p.CategoryName.Length >=51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Abstracts/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstracts
+{
+    public interface IRepository<T>
+    {
+        //CRUD
+        List<T> List();
+
+        void Insert(T p);
+        void Update(T p);
+        void Delete(T p);
+        //Filtreleme işlemi
+        List<T> List(Expression<Func<T, bool>> filter);
+
+    }
+}

78ce511f6ff3a9be57c20de141da75fd9c269ad0
Kubra Yon <kubrayon98@gmail.com>
2021-11-06 23:32:15 +0300

    added

---
+++ BlogSite/Controllers/HeadingController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EFHeadingDal());
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        WriterManager wm = new WriterManager(new EFWriterDal());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList() select new SelectListItem { Text = x.CategoryName, Value = x.CategoryID.ToString() }).ToList();
+            List<SelectListItem> valuewriter = (from x in wm.GetList() select new SelectListItem { Text = x.WriterName + " " + x.WriterSurname, Value = x.WriterID.ToString() }).ToList();
+            ViewBag.valuecat = valuecategory;
+            ViewBag.valuewrt = valuewriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BlogSite/Controllers/WriterController.cs
+++ BlogSite/Controllers/WriterController.cs
@@ -14,6 +14,7 @@ namespace BlogSite.Controllers
     {
         // GET: Writer
         WriterManager wm = new WriterManager(new EFWriterDal());
+        WriterValidator writervalidator = new WriterValidator();
         public ActionResult Index()
         {
             var Writervalues = wm.GetList();
@@ -27,11 +28,36 @@ namespace BlogSite.Controllers
         [HttpPost]
         public ActionResult AddWriter(Writer p)
         {
-            WriterValidator writervalidator = new WriterValidator();
+           
             ValidationResult results = writervalidator.Validate(p);
             if (results.IsValid)
             {
                 wm.Writeradd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writervalue = wm.GetByID(id);
+
+            return View(writervalue);
+        }
+        [HttpPost]
+        public ActionResult EditWriter(Writer p)
+        {
+            ValidationResult results = writervalidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.Writerupdate(p);
                 return RedirectToAction("Index");
             }
             else
